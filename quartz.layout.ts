04820aaa6902bae10954d6ba8b635f67ff33ac9b
macukadam <ugurcan.akpulat@gmail.com>
2026-01-13 22:07:10 +0100

    Quartz sync: Jan 13, 2026, 10:07 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Home: "/",
+      Tags: "/tags",
+      RSS: "/index.xml",
     },
   }),
 }
@@ -41,8 +42,12 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.RecentNotes({
+      limit: 5,
+      showTags: false,
+      title: "Latest",
+    }),
     Component.Backlinks(),
   ],
 }
